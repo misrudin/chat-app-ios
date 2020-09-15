281dc1865b48f91215e7b3e68a4441743bf2bb8b
misrudin <indooffice4@BMGs-MacBook-Pro-2.local>
2020-09-15 22:52:19 +0700

    initial commit

---
+++ Messages/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Messages
+//
+//  Created by BMG MacbookPro on 15/09/20.
+//  Copyright © 2020 BMG MacbookPro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
